0f12d411fb691dea2e20ddc8d9612ce5eb069a5e
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-06-11 15:22:39 +0200

    Hello

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hello name="Romain" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hello
   }
 }
 </script>
---
+++ hello-vue/src/components/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    Hello {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    name: String
+  }
+}
+</script>
